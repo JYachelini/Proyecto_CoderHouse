6b6bce6d33ef2995fa9575ca7b642196656e92a5
Julián Yachelini <juliyachelini@gmail.com>
2021-08-05 20:49:07 -0300

    agregando flechita

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 function alertModal(){
     alert('Gracias por tu mensaje!');
 }
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 100){
+        document.querySelector('.go-top-conteiner').classList.add('show');
+    }else{
+        document.querySelector('.go-top-conteiner').classList.remove('show');
+    }
+}
+
+document.querySelector('.go-top-conteiner').addEventListener('click', () =>{
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
